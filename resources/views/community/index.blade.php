b7588bb6958b8b98c6805043cabc46cf4b7b4cea
Luis Gordillo <92225965+LuisGordilloTadeo2003@users.noreply.github.com>
2023-09-26 19:09:52 +0100

    first commit

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<h1>Community</h1>
+<h1>Community bueno</h1>
 @foreach ($links as $link)
 <li>{{$link->title}} 
 <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small></li>
